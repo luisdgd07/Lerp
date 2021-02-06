59e071ea97df6a2d26ab70a8010457dc8c60d8c4
Tiago Cruz <tcruz@NPRT01.Home>
2021-02-06 21:51:38 +0100

    Initial

---
+++ app/Entities/Taxes.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Entities;
+
+use App\Entities\TaxRate;
+use Illuminate\Database\Eloquent\Model;
+
+class Taxes extends Model
+{
+    protected $guarded = [];
+    protected $table = 'taxes';
+
+    public function taxtype()
+    {
+        return $this->belongsTo(TaxRate::class, 'tax_type_id');
+    }
+}
